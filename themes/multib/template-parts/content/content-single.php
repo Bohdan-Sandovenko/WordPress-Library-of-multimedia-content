20ef59ec3f106e6ab7eaeb47557d46845616b8f6
--global <d.mudrak@mrbooster.com>
2025-04-23 19:46:58 +0200

    add account styles

--- themes/multib/template-parts/content/content-single.php
+++ themes/multib/template-parts/content/content-single.php
@@ -12,10 +12,14 @@
 
 <article id="post-<?php the_ID(); ?>">
 	<section class="py-5">
-		<div class="container py-md-5 post-container">
-			<?php echo get_the_post_thumbnail();?>
+		<div class="container post-container">
+			<a href="<?php echo home_url();?>" class="d-flex align-items-center mb-4 gap-2">
+				<i class="fas fa-chevron-left"></i>
+				Back
+			</a>
+			<?php echo get_the_post_thumbnail(null, 'full', array('class' => 'post-featured-image'));?>
 
-			<h1><?php echo get_the_title();?></h1>
+			<h1 class="my-4"><?php echo get_the_title();?></h1>
 			<?php the_content(); ?>
 			
 		</div>
